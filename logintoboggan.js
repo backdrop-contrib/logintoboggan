e9e7c9f3f92b43b4ee71c1de324e03d02f23d4fe
Chad Phillips <chad@apartmentlines.com>
2007-11-22 00:51:45 +0000

    make collapsible login block D6/jquery compatible. fix a few notices.

---
+++ logintoboggan.js
@@ -0,0 +1,19 @@
+// $Id$
+Drupal.behaviors.myBehavior = function (context) {
+  $("#toboggan-login:not(.toboggan-login-processed)", context).each(
+    function() {
+      $(this).addClass('toboggan-login-processed').hide();
+      Drupal.logintoboggan_toggleboggan();
+    }
+  );
+};
+
+Drupal.logintoboggan_toggleboggan = function() {
+  $("#toboggan-login-link").click(
+    function () {
+      $("#toboggan-login").slideToggle("fast");
+      this.blur();
+      return false;
+    }
+  );
+};
